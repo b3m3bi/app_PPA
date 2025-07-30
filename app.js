b890402e8fda6dc83edfc7a6b2e9ae17758106a0
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-07-29 19:56:26 -0600

    fix matrix rendering when editing code

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ function addDefinitionToTable(definitionTable, factor, code, definition, index) 
     row.innerHTML = `<td>${index + 1}</td> <td>${factor}</td> <td>${code}</td> <td>${definition}</td>`;
 
     let actionCell = row.insertCell();
+    let btnsContainer = document.createElement('div');
+    btnsContainer.classList.add('center-cell');
+    
     let deleteBtn = document.createElement('button');
     deleteBtn.addEventListener('click', () => {
         deleteDefinition(code);
@@ -27,23 +30,32 @@ function addDefinitionToTable(definitionTable, factor, code, definition, index) 
         renderTables();
         renderPlots();
     });
-    deleteBtn.textContent = 'Borrar';
-    actionCell.append(deleteBtn);
+
+    const svgDeleteIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>';
+    deleteBtn.innerHTML = svgDeleteIcon;
+    deleteBtn.classList.add("action-btn");
+    btnsContainer.append(deleteBtn);
     
     let editBtn = document.createElement('button');
     editBtn.addEventListener('click', () => {
         editDefinition(code);
+        showModal('edit');
     });
-    editBtn.textContent = 'Editar';
-    actionCell.append(editBtn);
+    
+    const svgEditIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>'
+    editBtn.innerHTML = svgEditIcon;
+    editBtn.classList.add("action-btn");
+    btnsContainer.append(editBtn);
+
+    actionCell.append(btnsContainer);
 }
 
 const factorInput = document.querySelector('#factor');
 const codeInput = document.querySelector('#code');
 const definitionInput = document.querySelector('#definition');
 
-const addBtn = document.querySelector('#add-btn');
-const updateBtn = document.querySelector('#update-btn');
+const addBtn = document.querySelector('#add-definition-btn');
+const updateBtn = document.querySelector('#update-definition-btn');
 
 function addDefinitionToNodeData(factor, code, definition){
     let node = {
@@ -58,6 +70,7 @@ addBtn.addEventListener('click', () => {
     addDefinitionToNodeData(factorInput.value, codeInput.value, definitionInput.value);
     renderTables();
     clearInputs();
+    closeModal()
 })
 
 function clearInputs() {
@@ -76,6 +89,17 @@ function deleteLinksUsingCode(code){
     linksUsingCode.forEach(link => linkData.splice(linkData.findIndex(l => l === link), 1));
 }
 
+function updateLinksUsingCode(oldCode, newCode){
+    for (let link of linkData) {
+        if (link.source === oldCode){
+            link.source = newCode;
+        }
+        if (link.target === oldCode){
+            link.target = newCode;
+        }
+    }
+}
+
 function editDefinition(code){
     let node = nodeData.filter(node => node.code === code).pop();
     factorInput.value = node.factor;
@@ -89,15 +113,16 @@ function updateDefinition(code){
     node.factor = factorInput.value;
     node.code = codeInput.value;
     node.definition = definitionInput.value;
+    updateLinksUsingCode(currentEditingCode, codeInput.value);
     renderTables();
 }
 
 let currentEditingCode = '';
-    updateBtn.addEventListener('click', () => {
+updateBtn.addEventListener('click', () => {
     updateDefinition(currentEditingCode);
     currentEditingCode = '';
     clearInputs();
-
+    closeModal();
 });
 
 ///////// Create influence matrix
@@ -386,3 +411,42 @@ function renderPlots() {
 
 renderTables();
 renderPlots();
+
+const definitionFormModal = document.querySelector('#definition-form-modal');
+const newDefinitionBtn = document.querySelector('#new-definition-btn');
+const closeBnt = document.querySelector(".close-btn");
+
+function showModal(mode) {
+    definitionFormModal.style.display = 'block';
+    if (mode === 'add'){
+        addBtn.style.display = 'block';
+    }
+    if (mode === 'edit'){
+        updateBtn.style.display = 'block';
+    }
+
+}
+
+function closeModal() {
+    definitionFormModal.style.display = 'none';
+    addBtn.style.display = 'none';
+    updateBtn.style.display = 'none';
+}
+
+newDefinitionBtn.addEventListener('click', () => {
+    factorInput.value = '';
+    codeInput.value = '';
+    definitionInput.value = '';
+    showModal('add');
+})
+
+window.addEventListener('click', event => {
+    if (event.target == closeBnt) {
+        definitionFormModal.style.display = 'none';
+    }
+});
+
+closeBnt.addEventListener('click', () => {
+    definitionFormModal.style.display = 'none';
+})
+
